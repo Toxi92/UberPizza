8befd1ccca501332dabb906b3f209dbcb35de931
Edgar.M <146377700+Toxi92@users.noreply.github.com>
2025-05-13 09:22:41 +0200

    Update bdd.pizza.php

--- Modele/bdd.pizza.php
+++ Modele/bdd.pizza.php
@@ -32,5 +32,5 @@ class QueryPizza{
 
 include_once("bd.auth.php");
 //$bdd = new Database('localhost','emolard_projet','emolard','kbbULD53-!');
-$bdd = new Database('mysql-molard.alwaysdata.net','molard_projet_pizza','molard','kbbULD53-!');
+$bdd = new Database();
 $QueryPizza = new QueryPizza($bdd);
